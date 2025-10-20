96c44311601505178f23ca644150d063b234ccd9
LvNixia <59402791+LvNixia@users.noreply.github.com>
2025-10-20 23:18:19 +0700

    Add files via upload

---
+++ hari4/index.js
@@ -0,0 +1,7 @@
+var data = require('./lib/math');
+
+var namadata = data.nama;
+var alamatdata = data.alamat;
+
+console.log("Nama data: ",namadata);
+console.log("Alamat data: ",alamatdata);
---
+++ hari4/lib/math.js
@@ -0,0 +1,4 @@
+var nama = "Akbar"
+var alamat = "Jl. Merdeka No. 1"
+
+module.exports = { nama: nama, alamat: alamat }
